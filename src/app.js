167096932cdc6fb578ca6594039ad6e44286245a
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-03-28 16:46:08 -0300

    Commit inicial

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+    return response.json('hello world')
+})
+
+app.listen(port, () => {
+    console.log('Servidor up')
+})
