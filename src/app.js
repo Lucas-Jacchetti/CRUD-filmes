fbc136411cbf0b4fe1a043ea96f16b7bc5a92a54
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-03-31 15:10:21 -0300

    teste com postman

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (request, response) => {
-    return response.json('hello world')
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        team: 'gremio',
+        rival: 'inter'
+    })
+    return res.json('hello world')
+})
+
+app.post('/team/:id', (req, res) => {
+    const { id } = req.params;
+    const { team } = req.body;
+    
+    res.send({
+        team: 'time com ${id} como id',
+    })
 })
 
 app.listen(port, () => {
