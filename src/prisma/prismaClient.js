d4d6122e67a5c53cc818185ac252c42d2a0c3b59
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-04-01 15:03:27 -0300

    Criação filmServices

--- src/prisma/prismaClient.js
+++ src/prisma/prismaClient.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/Client" //importação do prisma (?)
 const prisma = new PrismaClient(); //conexão com a base de dados
 
-module.exports = prisma;
+module.exports = prisma; //reusável em toda a aplicação
 
---
+++ src/services/filmServices.js
@@ -0,0 +1,38 @@
+//*CAMADA DE SERVIÇOS, QUE VÃO INTERAGIR COM O PRISMA
+
+const prisma = require('../prisma/prismaClient'); //conecta com o prisma (eu acho???)
+
+
+//retorna todos os filmes
+const getAll = async () => {
+    return await prisma.film.findMany(); 
+};
+
+//adicionar um novo filme
+const addFilm = async (data) => { //passa as informações do filme como parâmetro
+    return await prisma.film.create( {data} );
+};
+
+//retorna por id
+const getFilmById = async (id) => { //passa o id do filme como parametro 
+    return await prisma.film.findUnique({where: {id: id}, data}); //acha o filme em que o id for o id passado como parametro
+};
+
+//faz o update
+const updateFilm = async (id, data) => {
+    return await prisma.film.update({where: {id: id}});//acha o filme em que o id for o id passado como parametro e faz o update
+};
+
+//deleta por id
+const deleteFilm = async (id) => {
+    return await prisma.film.delete({where: {id: id}});
+}
+
+//exportando, ai outras partes da aplicação podem importar
+module.exports = {
+    getAll,
+    addFilm,
+    getFilmById,
+    updateFilm,
+    deleteFilm,
+};
